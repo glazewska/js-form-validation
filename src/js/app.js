dd1b9f9dd8463d2bfbf8eecea45824d32d6fc9ed
Anna Glazewska <anka.glazewska@gmail.com>
2018-09-06 18:28:30 +0200

    toggle error and success messages

--- src/js/app.js
+++ src/js/app.js
@@ -2,26 +2,56 @@
 
     console.log('Walidujemy form :)');
 
+    //     const formValidationConfig = {
+    //     "userName": [{
+    //         "name": "required",
+    //         "text": "Pole musi być wypełnione"
+    //     }, {
+    //         "name": "minLength",
+    //         "text": "Minimalna długość to: "
+    //         "value": 4
+    //     }],
+    //     "pin": [{
+    //         "name": "minLength",
+    //         "text": "Minimalna długość to: ",
+    //         "value": 5
+    //     }, {
+    //         "name": "maxLength",
+    //         "text": "Maxymalna długość to: ",
+    //         "value": 16
+    //     }]
+    // }
+
     const form = document.getElementById("form");
     const agreement1 = form.querySelector("#agreement-1");
     const agreement2 = form.querySelector("#agreement-2");
     const agreement3 = form.querySelector("#agreement-3");
 
-    const usernameRegex = new RegExp(/[a-z]+/g);
-    const pinRegex = new RegExp(/^[0-9]{1,8}$/g);
-    const amountRegex = new RegExp(/^[1-9][0-9]?$|^100$/g);
+    const usernameRegex = new RegExp(/[a-z]+/);
+    const pinRegex = new RegExp(/^[0-9]{1,8}$/);
+    const amountRegex = new RegExp(/^[1-9][0-9]?$|^100$/);
 
+    // var floatValue = parseFloat(toFloat);
+    //
+    // if (isNaN(floatValue)) {
+    //     notFloat();
+    // } else {
+    //     isFloat();
+    // }
+
+    //zły regex - przyjmuje spacje i cyfry
     function validateUsername() {
         const usernameValue = username.value;
         const usernameTest = usernameRegex.test(usernameValue);
 
         if (usernameValue === "") {
             addErrorMessage("username", "Musisz wpisać nazwę użytkownika");
-        } else if (usernameTest) {
-            addSuccessMessage("username", "Świetna nazwa użytkownika");
-        } else {
+        } else if (usernameTest === false) {
             addErrorMessage("username", "Niepoprawna nazwa użytkownika. Spróbuj ponownie");
+        } else {
+            addSuccessMessage("username", "Świetna nazwa użytkownika");
         }
+        console.log(usernameTest);
     }
 
     function validateEmail() {
@@ -62,6 +92,8 @@
         }
     }
 
+    //zrobić jedną funkcję do walidacji obu checkboxów z podanym argumentem, wg którego się sprawdza konkretny
+
     function validateRequiredCheckbox1() {
         const check1Value = agreement1.checked;
 
@@ -83,15 +115,32 @@
         }
     }
 
+    //usuwanie poprzednich komunikatów
+
     function addSuccessMessage(fieldName, messageText) {
         const emptyDiv = document.createElement("div");
         const fieldElement = form.querySelector(`[name='${fieldName}']`);
         const emptyDivText = document.createTextNode(messageText);
+        const invalidDiv = document.querySelector("invalid-feedback");
 
-        fieldElement.classList.add("is-valid");
-        fieldElement.parentNode.appendChild(emptyDiv);
-        emptyDiv.appendChild(emptyDivText);
-        emptyDiv.classList.add("valid-feedback");
+        // fieldElement.classList.add("is-valid");
+        // fieldElement.parentNode.appendChild(emptyDiv);
+        // emptyDiv.appendChild(emptyDivText);
+        // emptyDiv.classList.add("valid-feedback");
+
+        if (!fieldElement.classList.contains("is-valid") && !fieldElement.classList.contains("is-invalid")) {
+            console.log("nie ma klasy");
+            fieldElement.classList.add("is-valid");
+            fieldElement.parentNode.appendChild(emptyDiv);
+            emptyDiv.appendChild(emptyDivText);
+            emptyDiv.classList.add("valid-feedback");
+        } else if (fieldElement.classList.contains("is-invalid")) {
+            console.log("ma inna klasę");
+            fieldElement.classList.remove("is-invalid");
+            fieldElement.classList.add("is-valid");
+            invalidDiv.parentNode.removeChild(invalidDiv);
+        }
+
     }
 
     function addErrorMessage(fieldName, messageText) {
@@ -99,14 +148,26 @@
         const fieldElement = form.querySelector(`[name='${fieldName}']`);
         const emptyDivText = document.createTextNode(messageText);
 
-        fieldElement.classList.add("is-invalid");
-        fieldElement.parentNode.appendChild(emptyDiv);
-        emptyDiv.appendChild(emptyDivText);
-        emptyDiv.classList.add("invalid-feedback");
+        // fieldElement.classList.add("is-invalid");
+        // fieldElement.parentNode.appendChild(emptyDiv);
+        // emptyDiv.appendChild(emptyDivText);
+        // emptyDiv.classList.add("invalid-feedback");
+
+        if (!fieldElement.classList.contains("is-valid") && !fieldElement.classList.contains("is-invalid")) {
+            fieldElement.classList.add("is-invalid");
+            fieldElement.parentNode.appendChild(emptyDiv);
+            emptyDiv.appendChild(emptyDivText);
+            emptyDiv.classList.add("invalid-feedback");
+        } else if (fieldElement.classList.contains("is-valid")) {
+            fieldElement.classList.remove("is-valid").add("is-invalid");
+            fieldElement.parentNode.removeChild(emptyDiv);
+            emptyDiv.classList.remove("valid-feedback").add("invalid-feedback");
+        }
     }
 
     form.addEventListener("submit", (event) => {
         event.preventDefault();
+        console.log(username.value);
         validateUsername();
         validateEmail();
         validatePin();
